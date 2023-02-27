9c90e0e5a408dc4d0e71274dd65051669e83ac55
lucasgabriel-y <gabriel.lglg93@gmail.com>
2023-02-27 08:30:33 -0300

    Ajuste

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+  bucket = "bucket-lab"
 
   tags = {
-    Name        = "Teste"
+    Name        = "Bucket teste"
     Environment = "um"
   }
 }
