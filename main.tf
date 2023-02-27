0fd4453ee7a25f09404e8dc412c0b4b24189f2af
lucasgabriel-y <gabriel.lglg93@gmail.com>
2023-02-27 09:37:35 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,16 +2,24 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "bucket-lab"
+resource "aws_s3_bucket" "bucket-81246132" {
+  bucket = "bucket-lab81246132"
+
 
   tags = {
     Name        = "Bucket teste"
-    Environment = "um"
   }
 }
 
 resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+  bucket = aws_s3_bucket.bucket-81246132.id
+  acl    = "public"
+}
+
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket-81246132.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
